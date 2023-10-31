3b1be065456e0c27b9ca4dbe1190e93c8927531a
Artyom Serov(ws) <artyomserov@yandex.ru>
2023-10-31 14:45:38 +0500

    Added models to recipes.

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+
+class Ingredient(models.Model):
+    """Ингредиенты."""
+
+    pass
+
+
+class Tag(models.Model):
+    """Тэги."""
+
+    pass
+
+
+class Recipe(models.Model):
+    """Рецепты."""
+
+    pass
+
+
+class RecipesFavorite(models.Model):
+    """Избранные рецепты."""
+
+    pass
+
+
+class ShoppingCart(models.Model):
+    """Список покупок."""
+
+    pass
+
+
+class Follow(models.Model):
+    """Подписки на авторов рецептов."""
+
+    pass
