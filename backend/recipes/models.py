d6d4a4be0f4c714e830ef7f6d4d803dd2b241f71
Артём Серов (д) <artyomserov@yandex.ru>
2023-11-10 05:18:04 +0500

    Added models to recipes

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,37 +1,161 @@
+from django.core.validators import MinValueValidator
 from django.db import models
+from users.models import User
 
 
 class Ingredient(models.Model):
     """Ингредиенты."""
 
-    pass
+    name = models.CharField(
+        'Название ингредиента',
+        max_length=200,
+    )
+    measurement_unit = models.CharField(
+        'Единицы измерения',
+        max_length=200,
+    )
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Ингредиент'
+        verbose_name_plural = 'Ингредиенты'
+        constraints = [
+            models.UniqueConstraint(fields=['name', 'measurement_unit'],
+                                    name='unique ingredient')
+        ]
 
 
 class Tag(models.Model):
     """Тэги."""
 
-    pass
+    name = models.CharField(
+        'Название тега',
+        max_length=200,
+    )
+    color = models.CharField(
+        'Цвет в HEX',
+        max_length=7
+    )
+    slug = models.SlugField(
+        'Уникальный слаг',
+        unique=True,
+        max_length=200,
+    )
+
+    class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
 
 
 class Recipe(models.Model):
     """Рецепты."""
 
-    pass
+    tags = models.ManyToManyField(
+        Tag,
+        verbose_name='Список тегов',
+        related_name='recipes',
+    )
+    author = models.ForeignKey(
+        User,
+        verbose_name='Автор',
+        on_delete=models.CASCADE,
+        related_name='recipes',
+    )
+    ingredients = models.ManyToManyField(
+        Ingredient,
+        verbose_name='Список ингредиентов',
+        through='IngredientInRecipe',
+        related_name='recipes',
+    )
+    name = models.CharField(
+        'Название рецепта',
+        max_length=200
+    )
+    image = models.ImageField(
+        'Изображение',
+        upload_to='recipes/',
+    )
+    text = models.TextField(
+        'Описание рецепта'
+    )
+    cooking_time = models.PositiveSmallIntegerField(
+        'Время приготовления (в минутах)',
+        validators=(
+            MinValueValidator(
+                1, message='Время должно быть больше 1 минуты'),),
+    )
+
+
+class IngredientInRecipe:
+    """Ингредиенты для рецепта."""
+
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        related_name='amounts',
+        verbose_name='Рецепт'
+    )
+    ingredient = models.ForeignKey(
+        Ingredient,
+        on_delete=models.CASCADE,
+        related_name='amounts',
+        verbose_name='Ингредиент'
+    )
+    amount = models.PositiveSmallIntegerField(
+        'Количество ингредиента',
+        validators=(
+            MinValueValidator(
+                1, message='Количество не может быть меньше 1'),),
+    )
+
+    class Meta:
+        verbose_name = 'Количество ингредиента'
+        verbose_name_plural = 'Количество ингредиентов'
+        constraints = (
+            models.UniqueConstraint(
+                fields=('ingredient', 'recipe',),
+                name='unique ingredient amount',
+            ),
+        )
 
 
 class RecipesFavorite(models.Model):
     """Избранные рецепты."""
 
-    pass
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='favorites_user',
+        verbose_name='Пользователь',
+    )
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        related_name='favorites',
+        verbose_name='Рецепт',
+    )
 
 
 class ShoppingCart(models.Model):
     """Список покупок."""
 
-    pass
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='carts',
+        verbose_name='Пользователь',
+    )
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        related_name='carts',
+        verbose_name='Рецепт',
+    )
 
-
-class Follow(models.Model):
-    """Подписки на авторов рецептов."""
-
-    pass
+    class Meta:
+        verbose_name = 'Корзина'
+        verbose_name_plural = 'Корзина'
+        constraints = [
+            models.UniqueConstraint(fields=['user', 'recipe'],
+                                    name='unique shopping cart')
+        ]
