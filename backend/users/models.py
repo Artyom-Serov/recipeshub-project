aacfcc75d37c69697ef75a02d7f9c945097bc3af
Artyom Serov(ws) <artyomserov@yandex.ru>
2023-11-11 19:34:40 +0500

    Add user model

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    """Модель пользователя."""
+
+    username = models.CharField(
+        max_length=150,
+        unique=True,
+    )
+    first_name = models.CharField('Имя', max_length=150)
+    last_name = models.CharField('Фамилия', max_length=150)
+    email = models.EmailField('Почта', unique=True, max_length=150)
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+        ordering = ['id']
+
+    def __str__(self):
+        return self.username
