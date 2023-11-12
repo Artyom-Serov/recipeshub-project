af451be860361d50d6aa65e64e11a3b264ddf573
Артём Серов (д) <artyomserov@yandex.ru>
2023-11-12 11:45:32 +0500

    Added models to users

--- backend/users/models.py
+++ backend/users/models.py
@@ -5,18 +5,52 @@ from django.db import models
 class User(AbstractUser):
     """Модель пользователя."""
 
+    email = models.EmailField(
+        'Адрес электронной почты',
+        unique=True,
+        max_length=150,
+    )
     username = models.CharField(
         max_length=150,
         unique=True,
     )
     first_name = models.CharField('Имя', max_length=150)
     last_name = models.CharField('Фамилия', max_length=150)
-    email = models.EmailField('Почта', unique=True, max_length=150)
 
     class Meta:
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
         ordering = ['id']
 
+    def get_full_name(self):
+        return f'{self.first_name} {self.last_name}'
+
     def __str__(self):
-        return self.username
+        return self.email
+
+
+class Follow(models.Model):
+    """Модель подписки."""
+
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='follower',
+        verbose_name='Подписчик',
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='following',
+        verbose_name='Автор',
+    )
+
+    class Meta:
+        verbose_name = 'Подписка'
+        verbose_name_plural = 'Подписки'
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'author'],
+                name='unique follow',
+            )
+        ]
