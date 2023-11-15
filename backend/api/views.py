9b81a62faf0ac34889f126f033de6c3a0e023832
Артём Серов (д) <artyomserov@yandex.ru>
2023-11-15 06:19:03 +0500

    Added viewsets from recipes

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,119 @@
-from django.shortcuts import render
+from djoser.views import UserViewSet
+from rest_framework import status
+from rest_framework.generics import ListAPIView, get_object_or_404
+from rest_framework.permissions import (AllowAny, IsAuthenticated,
+                                        IsAuthenticatedOrReadOnly)
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 
-# Create your views here.
+from .pagination import CustomPagination
+from .serializers import (CustomUserSerializer, FollowSerializer,
+                          IngredientSerializer,
+                          TagSerializer)
+from recipes.models import (Ingredient, Recipe,
+                            Tag)
+from users.models import Follow, User
+
+
+class CustomUserViewSet(UserViewSet):
+    """
+    ViewSet для работы с пользователями.
+    """
+
+    queryset = User.objects.all()
+    serializer_class = CustomUserSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+
+class FollowViewSet(APIView):
+    """
+    APIView для добавления и удаления подписки на автора
+    """
+
+    serializer_class = FollowSerializer
+    permission_classes = [IsAuthenticated]
+    pagination_class = CustomPagination
+
+    def post(self, request, *args, **kwargs):
+        user_id = self.kwargs.get('user_id')
+        if user_id == request.user.id:
+            return Response(
+                {'error': 'Нельзя подписаться на себя'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        if Follow.objects.filter(
+                user=request.user,
+                author_id=user_id
+        ).exists():
+            return Response(
+                {'error': 'Вы уже подписаны на пользователя'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        author = get_object_or_404(User, id=user_id)
+        Follow.objects.create(
+            user=request.user,
+            author_id=user_id
+        )
+        return Response(
+            self.serializer_class(author, context={'request': request}).data,
+            status=status.HTTP_201_CREATED
+        )
+
+    def delete(self, request, *args, **kwargs):
+        user_id = self.kwargs.get('user_id')
+        get_object_or_404(User, id=user_id)
+        subscription = Follow.objects.filter(
+            user=request.user,
+            author_id=user_id
+        )
+        if subscription:
+            subscription.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response(
+            {'error': 'Вы не подписаны на пользователя'},
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
+
+class FollowListView(ListAPIView):
+    """
+    APIView для просмотра подписок.
+    """
+
+    serializer_class = FollowSerializer
+    permission_classes = [IsAuthenticated]
+    pagination_class = CustomPagination
+
+    def get_queryset(self):
+        return User.objects.filter(following__user=self.request.user)
+
+
+class TagsViewSet(ReadOnlyModelViewSet):
+    """
+    ViewSet для работы с тегами.
+    Добавить тег может администратор.
+    """
+    queryset = Tag.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = TagSerializer
+
+
+class IngredientsViewSet(ReadOnlyModelViewSet):
+    """
+    ViewSet для работы с ингредиентами.
+    Добавить ингредиент может администратор.
+    """
+    queryset = Ingredient.objects.all()
+    permission_classes = (AllowAny, )
+    serializer_class = IngredientSerializer
+    search_fields = ('^name',)
+
+
+class RecipeViewSet(ModelViewSet):
+    """
+    ViewSet для работы с рецептами.
+    Для анонимов разрешен только просмотр рецептов.
+    """
+    queryset = Recipe.objects.all()
+    pagination_class = CustomPagination
