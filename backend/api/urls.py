c18f54f3217bbaeaf35232a4f479ea53b4c1010a
Артём Серов (д) <artyomserov@yandex.ru>
2023-11-16 06:17:09 +0500

    Add urls

---
+++ backend/api/urls.py
@@ -0,0 +1,28 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from .views import (IngredientsViewSet, RecipeViewSet, TagsViewSet,
+                    CustomUserViewSet, FollowListView, FollowViewSet)
+
+router = DefaultRouter()
+router.register('users', CustomUserViewSet, basename='users')
+router.register('recipes', RecipeViewSet, basename='recipes')
+router.register('ingredients', IngredientsViewSet, basename='ingredients')
+router.register('tags', TagsViewSet, basename='tags')
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path(
+        'users/subscriptions/',
+        FollowListView.as_view(),
+        name='subscriptions'
+    ),
+    path(
+        'users/<int:user_id>/subscribe/',
+        FollowViewSet.as_view(),
+        name='subscribe'
+    ),
+    path('', include(router.urls)),
+    path('', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
+]
