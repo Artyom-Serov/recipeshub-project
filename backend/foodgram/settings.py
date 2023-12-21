0999d8c0cd3a876688b2b621770652a6e7ea3173
Artyom Serov(ws) <artyomserov@yandex.ru>
2023-12-21 06:25:40 +0500

    Fix import from .env

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -14,11 +14,11 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv
 
-load_dotenv()
+dotenv_path = Path(__file__).resolve().parent.parent.parent / 'env'
+load_dotenv(dotenv_path)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
