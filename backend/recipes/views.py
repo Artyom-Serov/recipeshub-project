b0a4feeeb058b715b62aaa2458325f2ed9115a43
Artyom Serov(ws) <artyomserov@yandex.ru>
2024-03-01 17:05:18 +0500

    Correct views.py in backend/recipes for PEP8

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -8,8 +8,7 @@ from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 
 from .models import (Ingredient, Recipe,
-                            Tag, RecipesFavorite, ShoppingCart,
-                            IngredientInRecipe)
+                            Tag, RecipesFavorite, ShoppingCart)
 from api.filters import IngredientSearchFilter, RecipeFilter
 from api.pagination import CustomPagination
 from api.permissions import IsAuthorOrReadOnly
