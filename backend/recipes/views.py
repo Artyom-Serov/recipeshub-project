c8cbdda619c7677a0c5d0db0a3e0222a7e23955f
Artyom-Serov <artyomserov@yandex.ru>
2024-03-24 11:10:56 +0500

    Correction POST-response for 'api/recipes/{id}/shopping_cart/'

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -73,16 +73,13 @@ class RecipeViewSet(ModelViewSet):
         """Общий метод для обработки POST-запросов."""
         data = {'user': request.user.id, 'recipe': pk}
         serializer = serializers(data=data, context={'request': request})
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-        # if serializer.is_valid():
-        #     try:
-        #         serializer.save()
-        #     except IntegrityError:
-        #         return Response(status=status.HTTP_400_BAD_REQUEST)
-        #     return Response(serializer.data, status=status.HTTP_201_CREATED)
-        # return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        if serializer.is_valid():
+            try:
+                serializer.save()
+            except IntegrityError:
+                return Response(status=status.HTTP_400_BAD_REQUEST)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     @staticmethod
     def delete_method_for_actions(request, pk, model):
